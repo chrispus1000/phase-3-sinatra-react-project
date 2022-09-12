58dc7c7d03cc0d1811b2d67cb924dfcf2d22cfd5
chrispus1000 <minrevac@gmail.com>
2022-09-13 01:34:05 +0300

    back end project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+  get '/words' do
+  
+    words.to_json
+  end
 end
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,20 +1,23 @@
 class WordsController < ApplicationController
 
-    get "/words" do
-        Word.all.to_json(include: [:comments])
+    get '/words' do
+        words = Word.all
+        words.to_json(include: [:comments])
+        
     end
+  
 
-    get "/words/random" do
+    get '/words/random' do
     word = Word.random
     word.to_json(include: [:comments])
     end
 
-    get "/words/:id" do
+    get '/words/:id' do
     word = Word.find(params[:id])
     word.to_json(include: [:comments])
     end
 
-    post "/words" do
+    post '/words' do
     new_word = Word.create(
         word: params[:words],
         user: params[:user]
